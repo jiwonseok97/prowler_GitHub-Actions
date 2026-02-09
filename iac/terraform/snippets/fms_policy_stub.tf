5702b688e150d51f526ce8dda4b669dad55c835a
jiwonseok97 <jjjws5670@gmail.com>
2026-02-09 17:54:27 +0900

    Fix FMS policy snippet required argument

--- iac/terraform/snippets/fms_policy_stub.tf
+++ iac/terraform/snippets/fms_policy_stub.tf
@@ -4,6 +4,7 @@ resource "aws_fms_policy" "example" {
   name        = "org-mandatory-policy"
   resource_type = "AWS::ElasticLoadBalancingV2::LoadBalancer"
   remediation_enabled = true
+  exclude_resource_tags = false
 
   security_service_policy_data {
     type = "WAFV2"
